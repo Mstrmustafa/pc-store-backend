772e6ee9332e6408a5695058358c28a5239fc9d3
Mustafa msameh <153681052+Mstrmustafa@users.noreply.github.com>
2026-09-07 14:23:30 +0300

    Add src/PcStore.Core/Common/Entity.cs

---
+++ src/PcStore.Core/Common/Entity.cs
@@ -0,0 +1,8 @@
+namespace PcStore.Core.Common;
+
+public abstract class Entity
+{
+    public Guid Id { get; protected set; } = Guid.NewGuid();
+    public DateTimeOffset CreatedAtUtc { get; protected set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset? UpdatedAtUtc { get; protected set; }
+}
