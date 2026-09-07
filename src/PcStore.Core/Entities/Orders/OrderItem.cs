9efedb0c73c6c3f61e2830c21f09f71504032ddb
Mustafa msameh <153681052+Mstrmustafa@users.noreply.github.com>
2026-09-07 14:23:48 +0300

    Add src/PcStore.Core/Entities/Orders/OrderItem.cs

---
+++ src/PcStore.Core/Entities/Orders/OrderItem.cs
@@ -0,0 +1,14 @@
+using PcStore.Core.Common;
+
+namespace PcStore.Core.Entities.Orders;
+
+public sealed class OrderItem : Entity
+{
+    private OrderItem() { }
+    public Guid OrderId { get; private set; }
+    public Guid ProductId { get; private set; }
+    public string ProductName { get; private set; } = string.Empty;
+    public string Sku { get; private set; } = string.Empty;
+    public decimal UnitPrice { get; private set; }
+    public int Quantity { get; private set; }
+}
