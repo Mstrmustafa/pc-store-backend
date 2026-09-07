a7e64c2b4ff0e80338d26e70fe45424c86cebfd6
Mustafa msameh <153681052+Mstrmustafa@users.noreply.github.com>
2026-09-07 14:23:16 +0300

    Align src/PcStore.DataAccess/Configurations/OrderConfiguration.cs with Core architecture

--- src/PcStore.DataAccess/Configurations/OrderConfiguration.cs
+++ src/PcStore.DataAccess/Configurations/OrderConfiguration.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using PcStore.Domain.Orders;
+using PcStore.Core.Entities.Orders;
 
 namespace PcStore.DataAccess.Configurations;
 
