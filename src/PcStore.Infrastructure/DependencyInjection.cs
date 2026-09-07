9e273f8a619c5fa15ba31fdf18b13209894fcf87
Mustafa msameh <153681052+Mstrmustafa@users.noreply.github.com>
2026-09-07 13:55:16 +0300

    Update src/PcStore.Infrastructure/DependencyInjection.cs for DataAccess separation

--- src/PcStore.Infrastructure/DependencyInjection.cs
+++ src/PcStore.Infrastructure/DependencyInjection.cs
@@ -1,19 +1,11 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using PcStore.Application.Abstractions;
-using PcStore.Infrastructure.Catalog;
-using PcStore.Infrastructure.Persistence;
 
 namespace PcStore.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        services.AddDbContext<PcStoreDbContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("PcStore")));
-        services.AddScoped<IProductQueries, ProductQueries>();
         return services;
     }
 }
