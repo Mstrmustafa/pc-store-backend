1b5244827013710fff67ea590cf3a6397390f3cb
Mustafa msameh <153681052+Mstrmustafa@users.noreply.github.com>
2026-09-07 13:55:23 +0300

    Add src/PcStore.DataAccess/Configurations/CatalogConfiguration.cs

---
+++ src/PcStore.DataAccess/Configurations/CatalogConfiguration.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PcStore.Domain.Catalog;
+
+namespace PcStore.DataAccess.Configurations;
+
+public sealed class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("products"); builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).HasMaxLength(200).IsRequired();
+        builder.Property(x => x.Sku).HasMaxLength(64).IsRequired();
+        builder.Property(x => x.Price).HasPrecision(18, 2);
+        builder.HasIndex(x => x.Sku).IsUnique();
+        builder.HasOne(x => x.Category).WithMany().HasForeignKey(x => x.CategoryId);
+    }
+}
+
+public sealed class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("categories"); builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).HasMaxLength(120).IsRequired();
+        builder.Property(x => x.Slug).HasMaxLength(140).IsRequired();
+        builder.HasIndex(x => x.Slug).IsUnique();
+    }
+}
+
+public sealed class BrandConfiguration : IEntityTypeConfiguration<Brand>
+{
+    public void Configure(EntityTypeBuilder<Brand> builder)
+    {
+        builder.ToTable("brands"); builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).HasMaxLength(120).IsRequired();
+        builder.HasIndex(x => x.Name).IsUnique();
+    }
+}
