04f40d587c48cb0213794585d6cd7234ee15fd88
Mustafa msameh <153681052+Mstrmustafa@users.noreply.github.com>
2026-09-07 14:24:04 +0300

    Replace Domain/Application with IFMS-style Core project

--- src/PcStore.Application/Abstractions/IProductQueries.cs
+++
@@ -1,9 +0,0 @@
-using PcStore.Application.Catalog;
-using PcStore.Application.Common;
-
-namespace PcStore.Application.Abstractions;
-
-public interface IProductQueries
-{
-    Task<PagedResult<ProductDto>> GetAsync(string? search, int page, int pageSize, CancellationToken cancellationToken);
-}
