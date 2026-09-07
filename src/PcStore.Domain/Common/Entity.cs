8af7f92ceb0c9b762a9308dca26366855e382961
Mustafa msameh <153681052+Mstrmustafa@users.noreply.github.com>
2026-09-07 14:23:51 +0300

    Replace Domain/Application with IFMS-style Core project

--- src/PcStore.Domain/Common/Entity.cs
+++
@@ -1,8 +0,0 @@
-namespace PcStore.Domain.Common;
-
-public abstract class Entity
-{
-    public Guid Id { get; protected set; } = Guid.NewGuid();
-    public DateTimeOffset CreatedAtUtc { get; protected set; } = DateTimeOffset.UtcNow;
-    public DateTimeOffset? UpdatedAtUtc { get; protected set; }
-}
